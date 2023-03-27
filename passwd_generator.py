8580bf0c9c0fbed4efd37712b2d1860592ce05b2
{G} gurraoptimus <63198393+gurraoptimus@users.noreply.github.com>
2023-03-27 18:02:11 +0200

    passwd_generator

--- passwd_generator.py
+++ passwd_generator.py
@@ -2,4 +2,6 @@ import random
 import string
 
 def generate_password(min_length, numbers=True, special_characters=True):
-    letters.
+    letters = string.ascii_letters
+    digits = string.digits
+    spec
