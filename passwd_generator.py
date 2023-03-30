0e3a12198befb5102fc32cf59c5d461a19825d7b
{G} gurraoptimus <63198393+gurraoptimus@users.noreply.github.com>
2023-03-30 20:17:28 +0200

    passwd-generator

--- passwd_generator.py
+++ passwd_generator.py
@@ -22,20 +22,22 @@ while not meets_criteria or len(pwd) < min_length:
     new_char = random.choice(characters)
     pwd += new_char
 
-if new_char in digits:
-    has_number = True
-elif new_char in special:
-    has_special = True
+    if new_char in digits:
+        has_number = True
+    elif new_char in special:
+        has_special = True
 
-meets_criteria = True
-if numbers:
+    meets_criteria = True
+    if numbers:
         meets_criteria = has_number
-if special_characters:
+    if special_characters:
         meets_criteria = meets_criteria and has_special
 
 return pwd
 
 
 min_length = int(input("Enter the minimum length: "))
-pwd = generate_password(10)
-print(pwd)
+has_number = input("Do you want to have numbers (y/n)? ").lower() == "y"
+has_number = input("Do you want to have special characters (y/n)? ").lower() == "y"
+pwd = generate_password(min_length, has_number, has_special)
+print("The generated password is:", pwd)
