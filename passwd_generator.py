3e43c9e8658d029f9a7b40ceaacf2a3b13d603de
{G} gurraoptimus <63198393+gurraoptimus@users.noreply.github.com>
2023-04-12 10:30:46 +0200

    passwd-generator
    while loop 404.

--- passwd_generator.py
+++ passwd_generator.py
@@ -6,13 +6,13 @@ def generate_password(min_length, numbers=True, special_characters=True):
     letters = string.ascii_letters
     digits = string.digits
     special = string.punctuation
-
+    
     characters = letters
     if numbers:
         characters += digits
     if special_characters:
         characters += special
-
+    
     pwd =""
     meets_criteria = False
     has_number = False
@@ -20,19 +20,18 @@ def generate_password(min_length, numbers=True, special_characters=True):
 
     while not meets_criteria or len(pwd) < min_length:
         new_char = random.choice(characters)
-    pwd += new_char
+        pwd += new_char
+        
+        if new_char in string.digits:
+            has_number = True
+        elif new_char in special:
+            has_special = True
 
-    if new_char in string.digits:
-        has_number = True
-    elif new_char in special:
-        has_special = True
-
-    meets_criteria = True
-    if numbers:
-        meets_critera = has_number
-    if special_characters:
-        meets_criteria = meets_criteria and has_special
-def pwd():
+        meets_criteria = True
+        if numbers:
+            meets_critera = has_number
+        if special_characters:
+            meets_criteria = meets_criteria and has_special
     return pwd
 
 
